b8dfb3586071ec48505e832d49451199783a971a
artem-ltv <litwinenkoartem1@gmail.com>
2021-04-16 19:59:54 +0300

    PingPong release V1

---
+++ Assets/Scripts/ButtonCloseSettings.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonCloseSettings : MonoBehaviour
+{
+    public GameObject ballColorSettings;
+    public GameObject buttonPlay;
+    public GameObject buttonSettings;
+    public GameObject buttonAny1;
+    public GameObject buttonAny2;
+    private void OnMouseUpAsButton()
+    {
+        ballColorSettings.SetActive(false);
+        buttonPlay.SetActive(true);
+        buttonSettings.SetActive(true);
+        buttonAny1.SetActive(true);
+        buttonAny2.SetActive(true);
+    }
+    public void OnMouseDown()
+    {
+        transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+    }
+    public void OnMouseUp()
+    {
+        transform.localScale = new Vector3(0.45f, 0.45f, 0.45f);
+    }
+}
---
+++ Assets/Scripts/ChoiceGreenBall.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChoiceGreenBall : MonoBehaviour
+{
+    public GameObject choiceGreen;
+    public GameObject choiceWhite;
+    public GameObject choicePink;
+    private void OnMouseUp()
+    {
+        PlayerPrefs.SetInt("Color", 1);
+    }
+    private void Update()
+    {
+        if (PlayerPrefs.GetInt("Color") == 1)
+        {
+            choiceGreen.SetActive(true);
+            choiceWhite.SetActive(false);
+            choicePink.SetActive(false);
+        }
+    }
+}
